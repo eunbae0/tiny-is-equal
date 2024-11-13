1b16b0778f6d3fd6e91d17529e4fc8e001d71694
EunBae Gong <geb9211@gmail.com>
2024-11-13 21:19:35 +0900

    init: first commit

---
+++ rslib.config.ts
@@ -0,0 +1,29 @@
+import type { LibConfig } from '@rslib/core';
+import { defineConfig } from '@rslib/core';
+
+const common: LibConfig = {
+  syntax: 'es2015',
+  output: {
+    minify: {
+      jsOptions: {
+        minimizerOptions: {
+          compress: true,
+        },
+      },
+    },
+  },
+};
+
+export default defineConfig({
+  lib: [
+    {
+      format: 'esm',
+      dts: true,
+      ...common,
+    },
+    {
+      format: 'cjs',
+      ...common,
+    },
+  ],
+});
